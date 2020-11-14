9ddfd0074c72eda13983f8a6d737383d3474d957
Alejandro Garrido Mota <garridomota@gmail.com>
2020-11-14 12:09:59 +0000

    WIP: Trying to execute the initial load of the calendar.

--- frontend/pages/Calendar.vue
+++ frontend/pages/Calendar.vue
@@ -41,60 +41,104 @@ import { mapActions, mapGetters } from 'vuex'
 export default {
   data: () => ({
     focus: '',
-    events: [],
-    colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
-    names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
+    events: []
   }),
-  mounted () {
+
+  mounted() {
     this.$refs.calendar.checkChange()
     this.$store.dispatch('measures/loadMeasures')
     this.$store.dispatch('fluids/statsFluids')
   },
 
-  methods: {
-    ...mapGetters('measures', ['getByMonth' ]),
+  created() {
+    this.unsubscribe = this.$store.subscribe((mutation, state) => {
+      const events = []
+      var end = new Date()
+      var start = new Date(end.getFullYear(), end.getMonth(), 1)
 
-    viewDay ({ date }) {
+      if (mutation.type === 'measures/initMeasures') {
+        // this.$store.getters['measures/getByMonth'](start, end).forEach(data => {
+        //   events.push({
+        //     name: `Checked-in Weight: ${data.value}`,
+        //     start: new Date(`${data.date}T00:00:00`),
+        //     end: new Date(`${data.date}T23:59:59`),
+        //     color: 'orange',
+        //     timed: false
+        //   })
+        // })
+      }
+      
+      if (mutation.type === 'fluids/statsFluids') {
+       // this.$store.getters['fluids/getByMonth'](start, end).forEach(data => {
+       //   events.push({
+       //     name: `Total Fluids: ${data.value} ml`,
+       //     start: new Date(`${data.date}T00:00:00`),
+       //     end: new Date(`${data.date}T23:59:59`),
+       //     color: 'cyan',
+       //     timed: false
+       //   })
+       // })
+      }
+
+      //this.events = events
+    })
+  },
+
+  // TODO: Do I need this within beforeDestroy?
+  beforeDestroy() {
+    this.unsubscribe()
+  },
+
+  methods: {
+    ...mapGetters('measures', ['getByMonth']),
+
+    viewDay({ date }) {
       this.focus = date
       this.type = 'day'
     },
-    getEventColor (event) {
+    getEventColor(event) {
       return event.color
     },
-    setToday () {
+    setToday() {
       this.focus = ''
     },
-    prev () {
+    prev() {
       this.$refs.calendar.prev()
     },
-    next () {
+    next() {
       this.$refs.calendar.next()
     },
-    updateRange ({ start, end }) {
+    updateRange({ start, end }) {
       const events = []
       
-      this.$store.getters['fluids/getByMonth'](start, end).forEach(data => {
+      console.log("Tengo:", start)
+      console.log(end)
+      
+      var startDate = new Date(start.date)
+      var endDate = new Date(end.date)
+
+      this.$store.getters['fluids/getByMonth'](startDate, endDate).forEach(data => {
         events.push({
           name: `Total Fluids: ${data.value} ml`,
           start: new Date(`${data.date}T00:00:00`),
           end: new Date(`${data.date}T23:59:59`),
           color: 'cyan',
-          timed: false,
+          timed: false
         })
-      });
+      })
 
-      this.$store.getters['measures/getByMonth'](start, end).forEach(data => {
+      this.$store.getters['measures/getByMonth'](startDate, endDate).forEach(data => {
         events.push({
           name: `Checked-in Weight: ${data.value}`,
           start: new Date(`${data.date}T00:00:00`),
           end: new Date(`${data.date}T23:59:59`),
           color: 'orange',
-          timed: false,
+          timed: false
         })
-      });
+      })
 
       this.events = events
-    },
-  },
+    }
+  }
 }
 </script>
