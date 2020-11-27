06860967da973ddda863a8853cc664d6928cf226
Alejandro Garrido Mota <garridomota@gmail.com>
2020-11-27 22:54:24 +0000

    Calendar ready, loading everything from stats vuex module.

--- frontend/pages/Calendar.vue
+++ frontend/pages/Calendar.vue
@@ -46,41 +46,53 @@ export default {
 
   mounted() {
     this.$refs.calendar.checkChange()
-    this.$store.dispatch('measures/loadMeasures')
-    this.$store.dispatch('fluids/statsFluids')
+    this.$store.dispatch('stats/pullAll')
   },
 
   created() {
+    const events = []
+
     this.unsubscribe = this.$store.subscribe((mutation, state) => {
-      const events = []
       var end = new Date()
       var start = new Date(end.getFullYear(), end.getMonth(), 1)
 
-      if (mutation.type === 'measures/initMeasures') {
-        // this.$store.getters['measures/getByMonth'](start, end).forEach(data => {
-        //   events.push({
-        //     name: `Checked-in Weight: ${data.value}`,
-        //     start: new Date(`${data.date}T00:00:00`),
-        //     end: new Date(`${data.date}T23:59:59`),
-        //     color: 'orange',
-        //     timed: false
-        //   })
-        // })
+      if (mutation.type === 'stats/measures') {
+        this.$store.getters['stats/getMeasuresByMonth'](start, end).forEach(data => {
+          events.push({
+            name: `Checked-in Weight: ${data.value}`,
+            start: new Date(`${data.date}T00:00:00`),
+            end: new Date(`${data.date}T23:59:59`),
+            color: 'orange',
+            timed: false
+          })
+        })
       }
       
-      if (mutation.type === 'fluids/statsFluids') {
-       // this.$store.getters['fluids/getByMonth'](start, end).forEach(data => {
-       //   events.push({
-       //     name: `Total Fluids: ${data.value} ml`,
-       //     start: new Date(`${data.date}T00:00:00`),
-       //     end: new Date(`${data.date}T23:59:59`),
-       //     color: 'cyan',
-       //     timed: false
-       //   })
-       // })
+      if (mutation.type === 'stats/food') {
+        this.$store.getters['stats/getFoodByMonth'](start, end).forEach(data => {
+          events.push({
+            name: `Total Calories: ${data.calories}`,
+            start: new Date(`${data.date}T00:00:00`),
+            end: new Date(`${data.date}T23:59:59`),
+            color: 'red',
+            timed: false
+          })
+        })
+      }
+      
+      if (mutation.type === 'stats/fluids') {
+        this.$store.getters['stats/getFluidsByMonth'](start, end).forEach(data => {
+          events.push({
+            name: `Total Fluids: ${data.value} ml`,
+            start: new Date(`${data.date}T00:00:00`),
+            end: new Date(`${data.date}T23:59:59`),
+            color: 'cyan',
+            timed: false
+          })
+        })
       }
 
-      //this.events = events
+      this.events = events
     })
   },
 
@@ -90,8 +102,6 @@ export default {
   },
 
   methods: {
-    ...mapGetters('measures', ['getByMonth']),
-
     viewDay({ date }) {
       this.focus = date
       this.type = 'day'
@@ -114,7 +124,7 @@ export default {
       var startDate = new Date(start.date)
       var endDate = new Date(end.date)
 
-      this.$store.getters['fluids/getByMonth'](startDate, endDate).forEach(data => {
+      this.$store.getters['stats/getFluidsByMonth'](startDate, endDate).forEach(data => {
         events.push({
           name: `Total Fluids: ${data.value} ml`,
           start: new Date(`${data.date}T00:00:00`),
@@ -123,8 +133,18 @@ export default {
           timed: false
         })
       })
+      
+      this.$store.getters['stats/getFoodByMonth'](startDate, endDate).forEach(data => {
+        events.push({
+          name: `Total Calories: ${data.calories}`,
+          start: new Date(`${data.date}T00:00:00`),
+          end: new Date(`${data.date}T23:59:59`),
+          color: 'red',
+          timed: false
+        })
+      })
 
-      this.$store.getters['measures/getByMonth'](startDate, endDate).forEach(data => {
+      this.$store.getters['stats/getMeasuresByMonth'](startDate, endDate).forEach(data => {
         events.push({
           name: `Checked-in Weight: ${data.value}`,
           start: new Date(`${data.date}T00:00:00`),
