cccb4ae655dbcda4b4ed9b42ba263f398dbe3db6
Alejandro Garrido Mota <garridomota@gmail.com>
2020-12-26 01:21:39 +0000

    update User endpoint

--- internal/api/user.go
+++ internal/api/user.go
@@ -8,6 +8,7 @@ import (
 
 	db "github.com/librefitness/librefitness/internal/database"
 	"github.com/librefitness/librefitness/internal/serializers"
+	"github.com/librefitness/librefitness/internal/validators"
 )
 
 type login struct {
@@ -27,4 +28,29 @@ func userInfo(c *gin.Context) {
 	response := serializers.UserResponse(&user)
 
 	c.JSON(http.StatusOK, response)
+}
+
+func updateUser(c *gin.Context) {
+	claims := jwt.ExtractClaims(c)
+
+	user, err := db.FindOneUser(&db.User{Username: claims["id"].(string)})
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	userValidator := validators.NewUserValidatorFillWith(user)
+	if err := userValidator.Bind(c); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	userValidator.UserDb.ID = user.ID
+
+	if err := db.SaveOne(&userValidator.UserDb); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"error (db)": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
 }
